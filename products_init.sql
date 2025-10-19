ea9d95f464e3782cbec5b17ecd0251cef093757f
Roman Sadovskiy <s03roman@mail.ru>
2025-10-19 23:39:44 +0300

    16584

--- products_init.sql
+++ products_init.sql
@@ -2,14 +2,14 @@
 USE Products;
 
 -- Создание таблицы Product
-CREATE TABLE IF NOT EXISTS Product (
+CREATE TABLE IF NOT EXISTS Products (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
     Price DECIMAL(18,2) NOT NULL
 );
 
 -- Начальные данные
-INSERT INTO Product (Name, Price) VALUES
+INSERT INTO Products (Name, Price) VALUES
 ('Apple', 1.20),
 ('Banana', 0.80),
 ('Orange', 1.50),
